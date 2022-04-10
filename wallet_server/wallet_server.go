8717fbe656aff95eeed1240ffa99d4bbe0180990
numarako <s12257010@gmail.com>
2022-04-10 20:13:14 +0900

    json decode

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"block/utils"
 	"block/wallet"
+	"encoding/json"
 	"html/template"
 	"io"
 	"log"
@@ -61,7 +62,27 @@ func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
 func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
-		io.WriteString(w, string(utils.JsonStatus("success")))
+		// フロントから受け取ったJSON情報をstruct(ポインタ)に入れ、structの実態をdecodeすることでJSONの中身を配列として取り出す
+		decoder := json.NewDecoder(req.Body)
+		var t wallet.TransactionRequest
+		err := decoder.Decode(&t)
+		if err != nil {
+			log.Printf("ERROR: %v", err)
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		if !t.Validate() {
+			log.Println("ERROR: missing field(s)")
+			io.WriteString(w, string(utils.JsonStatus("fail")))
+			return
+		}
+		/*
+			fmt.Println(*t.SenderPublicKey)
+			fmt.Println(*t.SenderPrivateKey)
+			fmt.Println(*t.SenderBlockchainAddress)
+			fmt.Println(*t.RecipientBlockchainAddress)
+			fmt.Println(*t.Value)
+		*/
 	default:
 		w.WriteHeader(http.StatusBadRequest)
 		log.Println("ERROR: Invalid HTTP Method")
